61b34ebe06ef7e473e8348ad008a685c08c216fd
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2024-02-10 19:37:18 +0800

    Allow setting rate limitation for API endpoints

--- server/types.go
+++ server/types.go
@@ -5,8 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const CAPACITY = 30 // Capacity for rate limiter to prevent from being attacked
-
 type ApiServices interface {
 	RegisterModule(rg *gin.RouterGroup, options *app.ServerOptions)
 }
