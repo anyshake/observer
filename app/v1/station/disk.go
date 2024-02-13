07ab91a685fff793e238f0bc6970e283e57d9673
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2024-02-13 15:47:46 +0800

    Show disk usage of current working directory

--- app/v1/station/disk.go
+++ app/v1/station/disk.go
@@ -1,27 +1,26 @@
 package station
 
-import "github.com/shirou/gopsutil/disk"
+import (
+	"os"
+
+	"github.com/shirou/gopsutil/disk"
+)
 
 func getDisk() diskModel {
-	partitions, err := disk.Partitions(false)
+	cwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 
-	disks := make([]diskModel, 0)
-	for _, partition := range partitions {
-		usage, err := disk.Usage(partition.Mountpoint)
-		if err != nil {
-			panic(err)
-		}
-
-		disks = append(disks, diskModel{
-			Total:   usage.Total,
-			Free:    usage.Free,
-			Used:    usage.Used,
-			Percent: usage.UsedPercent,
-		})
+	usage, err := disk.Usage(cwd)
+	if err != nil {
+		panic(err)
 	}
 
-	return disks[0]
+	return diskModel{
+		Total:   usage.Total,
+		Free:    usage.Free,
+		Used:    usage.Used,
+		Percent: usage.UsedPercent,
+	}
 }
