3b8b46cf2e7618be8df8c9debb3b8f278650970c
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2024-10-13 03:15:34 +0800

    refactor: Record sequence numbers to file to reduce initialization time

--- services/miniseed/start.go
+++ services/miniseed/start.go
@@ -1,15 +1,11 @@
 package miniseed
 
 import (
-	"fmt"
-	"os"
-	"strconv"
 	"sync"
 
 	"github.com/anyshake/observer/drivers/explorer"
 	"github.com/anyshake/observer/services"
 	"github.com/anyshake/observer/utils/logger"
-	"github.com/bclswl0827/mseedio"
 )
 
 func (m *MiniSeedService) Start(options *services.Options, waitGroup *sync.WaitGroup) {
@@ -44,9 +40,9 @@ func (m *MiniSeedService) Start(options *services.Options, waitGroup *sync.WaitG
 	m.locationCode = options.Config.Stream.Location
 	m.channelPrefix = options.Config.Stream.Channel
 	m.miniseedSequence = map[string]int{
-		explorer.EXPLORER_CHANNEL_CODE_Z: 0,
-		explorer.EXPLORER_CHANNEL_CODE_E: 0,
-		explorer.EXPLORER_CHANNEL_CODE_N: 0,
+		explorer.EXPLORER_CHANNEL_CODE_Z: 1,
+		explorer.EXPLORER_CHANNEL_CODE_E: 1,
+		explorer.EXPLORER_CHANNEL_CODE_N: 1,
 	}
 	m.cleanUpCountDown = MINISEED_CLEANUP_INTERVAL
 
@@ -61,39 +57,17 @@ func (m *MiniSeedService) Start(options *services.Options, waitGroup *sync.WaitG
 	m.writeBufferCountDown = m.writeBufferInterval
 	m.miniseedBuffer = make([]explorer.ExplorerData, m.writeBufferInterval)
 
-	// Get sequence number if file exists
-	currentTime := options.TimeSource.Get()
-	for _, channelCode := range []string{
-		explorer.EXPLORER_CHANNEL_CODE_Z,
-		explorer.EXPLORER_CHANNEL_CODE_E,
-		explorer.EXPLORER_CHANNEL_CODE_N,
-	} {
-		channelName := fmt.Sprintf("%s%s", m.channelPrefix, channelCode)
-		filePath := m.getFilePath(channelName, currentTime)
-		_, err := os.Stat(filePath)
-		if err == nil {
-			logger.GetLogger(m.GetServiceName()).Infof("reading existing record from %s", filePath)
-
-			// Get last sequence number from file
-			var ms mseedio.MiniSeedData
-			err := ms.Read(filePath)
-			if err != nil {
-				logger.GetLogger(m.GetServiceName()).Errorln(err)
-				continue
-			}
-			if len(ms.Series) > 0 {
-				lastRecord := ms.Series[len(ms.Series)-1]
-				lastSeqNum := lastRecord.FixedSection.SequenceNumber
-				n, err := strconv.Atoi(lastSeqNum)
-				if err != nil {
-					logger.GetLogger(m.GetServiceName()).Errorln(err)
-					continue
-				}
-
-				// Set current sequence number
-				m.miniseedSequence[channelCode] = n
-				logger.GetLogger(m.GetServiceName()).Infof("starting %s from last record, sequence %d", channelName, n)
-			}
+	// Try to get sequence numbers
+	seqNums, err := m.readSequence()
+	if err == nil {
+		for channelCode, seqNum := range seqNums {
+			m.miniseedSequence[channelCode] = seqNum
+			logger.GetLogger(m.GetServiceName()).Infof(
+				"starting %s%s from last record, sequence %06d",
+				m.channelPrefix,
+				channelCode,
+				seqNum,
+			)
 		}
 	}
 
