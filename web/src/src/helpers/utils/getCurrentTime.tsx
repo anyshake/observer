2c68a5d2172ac13f369a40cf47e634b543cbf95a
Joshua Lee <24319042+bclswl0827@users.noreply.github.com>
2025-05-01 03:00:58 +0800

    chore: Remove unused functions

--- web/src/src/helpers/utils/getCurrentTime.tsx
+++
@@ -1,8 +0,0 @@
-export const getCurrentTime = (baseTime: number, refTime: number) => {
-    if (!baseTime || !refTime) {
-        return Date.now();
-    }
-
-    const elapsed = Date.now() - refTime;
-    return baseTime + elapsed;
-};
